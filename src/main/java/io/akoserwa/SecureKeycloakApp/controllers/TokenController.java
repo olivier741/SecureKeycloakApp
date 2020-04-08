ba71cbf70d2d1eeec6677edef11ccb4261dc5f9e
olivier741 <oliviertatsink@gmail.com>
2020-04-08 14:51:30 +0100

    keycloak spring boot

---
+++ src/main/java/io/akoserwa/SecureKeycloakApp/controllers/TokenController.java
@@ -0,0 +1,20 @@
+package io.akoserwa.SecureKeycloakApp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TokenController {
+
+
+    @RequestMapping("/token")
+    public String getToken(){
+        return JWTUtil.getJWTToken();
+    }
+    
+    @RequestMapping("/security")
+    public String getProducts() {
+       return "securend";
+    }
+
+}
